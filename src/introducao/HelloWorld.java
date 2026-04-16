2f1dc0f4839c3424801c99b81e4dae43d89534df
Jessica Aquino <aquinolobobr1811@gmail.com>
2026-04-16 20:38:46 -0300

    introdução a variáveis e constantes em Java!

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,12 @@
+package introducao; //indica onde esta 
+
+public class HelloWorld { //indica o tipo que é e seu nome
+
+	public static void main(String[] args) { //metodo princirpal que executa o código
+		// lógica fique por aqui
+		
+		System.out.print("Hello World"); //indentico ao escreva do portugol
+
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,25 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		//variaveis
+		
+		int idade = 25; //numero inteiro 
+		String nome = "Lillian"; //caracteres letras 
+		float graus = 20.0f; //ponto fluante, aceita decimal até 32bits
+		boolean resposta; //verdadeiro ou falso
+		
+		//constantes
+		//Boa prática utilizar maisculo para constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(nome);
+		System.out.println(QUANTIDADE);
+		System.out.println(graus);
+	}
+
+}
+
